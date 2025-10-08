1231f3422d0130478b1718de3f66103ab4f21192
Alok Kshirsagar <alok.kshirsagar@accelya.com>
2025-10-08 17:28:36 +0530

    add files

--- src/main/java/com/travelsync/config/SecurityConfig.java
+++ src/main/java/com/travelsync/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.travelsync.config;
 
+
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
